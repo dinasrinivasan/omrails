4a5ca1ae8ab228ac7e1b5c586c6531b951b67f43
Dina Srinivasan <dina.s@effidia.com>
2013-08-10 07:24:29 -0400

    Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Omrails::Application.routes.draw do
+  resources :posts
+
+
+  get 'about' => 'Pages#about'
+  root :to => 'Pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
